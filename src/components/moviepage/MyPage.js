4dc027e6cc370b3a194f3268f2511676aaa39e4e
kth <ghot4484@naver.com>
2024-06-27 17:48:11 +0900

    탑버튼 제거 최종 머지

--- src/components/moviepage/MyPage.js
+++ src/components/moviepage/MyPage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Pagination from './Paging.js';
-import TopButton from './TopButton.js';
 import { useNavigate } from "react-router-dom";
 import "./MyPage.css";
 
